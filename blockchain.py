1b2a1f555f3b2708f71bed6153fc6c4fb0f43c6d
Pedro Gabriel Amorim Soares <pedrogabrielbhz@gmail.com>
2021-06-07 19:58:59 -0300

    node network initial steps

--- blockchain.py
+++ blockchain.py
@@ -17,6 +17,7 @@ class Blockchain:
         self.chain = [genesis_block]
         self.__open_transactions = []
         self.hosting_node = hosting_node_id
+        self.__peer_nodes = set()
         self.load_data()
 
     @property
@@ -53,7 +54,7 @@ class Blockchain:
                     updated_blockchain.append(updated_block)
                 self.chain = updated_blockchain
 
-                self.__open_transactions = json.loads(file_content[1])
+                self.__open_transactions = json.loads(file_content[1][:-1])
                 updated_transactions = []
                 for tx in self.__open_transactions:
                     updated_transaction = Transaction(
@@ -61,6 +62,8 @@ class Blockchain:
                     )
                     updated_transactions.append(updated_transaction)
                 self.__open_transactions = updated_transactions
+                peer_nodes = json.loads(file_content[2])
+                self.__peer_nodes = set(peer_nodes)
         except (IOError, IndexError):
             pass
 
@@ -80,6 +83,8 @@ class Blockchain:
                 f.write('\n')
                 saveable_tx = [tx.__dict__ for tx in self.__open_transactions]
                 f.write(json.dumps(saveable_tx))
+                f.write('\n')
+                f.write(json.dumps(list(self.__peer_nodes)))
         except IOError:
             print('Saving failed!')
 
@@ -183,3 +188,23 @@ class Blockchain:
         self.__open_transactions = []
         self.save_data()
         return block
+
+    def add_peer_node(self, node):
+        """Add a new node to the peer node set.
+        Args:
+            node: The node URL which should be added.
+        """
+        self.__peer_nodes.add(node)
+        self.save_data()
+
+    def remove_peer_node(self, node):
+        """Remove a node to the peer node set.
+        Args:
+            node: The node URL which should be added.
+        """
+        self.__peer_nodes.discard(node)
+        self.save_data()
+        
+    def get_peer_nodes(self):
+        """Return a list of all connected peer nodes."""
+        return list(self.__peer_nodes)
