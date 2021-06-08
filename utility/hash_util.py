4d74bccc577f11fb734a8c3af114461a0ec58b29
Pedro Gabriel Amorim Soares <pedrogabrielbhz@gmail.com>
2021-06-08 10:45:21 -0300

    copycat wrapper

--- utility/hash_util.py
+++ utility/hash_util.py
@@ -1,6 +1,7 @@
 import hashlib as hl
 import json
 
+# __all__ = ['hash_string_256', 'hash_block']
 
 def hash_string_256(string):
     """Create a SHA256 hash for a given input string.
@@ -12,12 +13,11 @@ def hash_string_256(string):
 
 
 def hash_block(block):
-    """Hash a block and returns a string representation of it.
+    """Hashes a block and returns a string representation of it.
 
     Arguments:
         :block: The block that should be hashed.
     """
     hashable_block = block.__dict__.copy()
-    hashable_block['transactions'] = [tx.to_ordered_dict()
-                                      for tx in hashable_block['transactions']]
+    hashable_block['transactions'] = [tx.to_ordered_dict() for tx in hashable_block['transactions']]
     return hash_string_256(json.dumps(hashable_block, sort_keys=True).encode())
